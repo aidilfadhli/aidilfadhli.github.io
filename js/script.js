5b7513ce456e29a420c0705d2896c8a4beb3abed
aidilfadhli <aidilfadhliawaludin.2012@gmail.com>
2022-07-27 22:09:30 +0700

    fix

--- js/script.js
+++ js/script.js
@@ -22,3 +22,20 @@ function AccessClipboardData() {
   } catch (err) {}
 }
 setInterval('AccessClipboardData()', 300);
+
+function copyToClipboard() {
+  var aux = document.createElement('input');
+  aux.setAttribute('value', 'print screen disabled!');
+  document.body.appendChild(aux);
+  aux.select();
+  document.execCommand('copy');
+  // Remove it from the body
+  document.body.removeChild(aux);
+  alert('Print screen disabled!');
+}
+
+$(window).keyup(function (e) {
+  if (e.keyCode == 44) {
+    copyToClipboard();
+  }
+});
