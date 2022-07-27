645295c0ed957b459f87a12c933ef1d31d90e552
aidilfadhli <aidilfadhliawaludin.2012@gmail.com>
2022-07-27 22:10:54 +0700

    fix

--- js/script.js
+++ js/script.js
@@ -1,28 +1,3 @@
-document.addEventListener('keyup', function (e) {
-  var keyCode = e.keyCode ? e.keyCode : e.which;
-  if (keyCode == 44) {
-    stopPrntScr();
-  }
-});
-function stopPrntScr() {
-  var inpFld = document.createElement('input');
-  inpFld.setAttribute('value', '.');
-  inpFld.setAttribute('width', '0');
-  inpFld.style.height = '0px';
-  inpFld.style.width = '0px';
-  inpFld.style.border = '0px';
-  document.body.appendChild(inpFld);
-  inpFld.select();
-  document.execCommand('copy');
-  inpFld.remove(inpFld);
-}
-function AccessClipboardData() {
-  try {
-    window.clipboardData.setData('text', 'Access   Restricted');
-  } catch (err) {}
-}
-setInterval('AccessClipboardData()', 300);
-
 function copyToClipboard() {
   var aux = document.createElement('input');
   aux.setAttribute('value', 'print screen disabled!');
